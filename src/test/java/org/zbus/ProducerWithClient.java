7850bc307fe72b873515032a1bb2863cd25927e0
少帮主 <44194462@qq.com>
2014-08-03 18:47:43 +0800

    C example and dist added

--- src/test/java/org/zbus/ProducerWithClient.java
+++ src/test/java/org/zbus/ProducerWithClient.java
@@ -12,11 +12,11 @@ public class ProducerWithClient {
 
 	public static void main(String[] args) throws Exception {   
 		//1) 创建到ZbusServer的链接
-		final RemotingClient client = new RemotingClient("127.0.0.1", 15555); 
+		final RemotingClient client = new RemotingClient("127.0.0.1:15555"); 
 		
 		//2) 包装为生产者，client生命周期不受Producer控制，因此Producer是个轻量级对象
 		Producer producer = new Producer(client, "MyMQ"); 
-		final int count = 10; //重复发送场景
+		final int count = 10000; //重复发送场景
 		final AtomicInteger idx = new AtomicInteger();
 		for(int i=0;i<count;i++){   
 			
