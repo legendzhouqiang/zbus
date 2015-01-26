5466b2fc45b47afb7d5b57d0352e2013975e3907
少帮主 <44194462@qq.com>
2015-01-26 11:32:29 +0800

    update zbus-dist shell and bat

--- zbus-dist/ha/track.bat
+++ zbus-dist/ha/track.bat
@@ -2,4 +2,4 @@ REM SET JAVA_HOME=%JAVA_HOME%\bin\
 SET JAVA_OPTS=-Dfile.encoding=UTF-8 -server -Xms1024m -Xmx4096m -XX:+UseParallelGC
 SET MAIN_CLASS=org.zbus.server.TrackServer
 SET MAIN_OPTS=-p 16666
-START /B java %JAVA_OPTS% -cp "../lib/*;../lib/logging/*;../*;" %MAIN_CLASS% %MAIN_OPTS% 
+START /B java %JAVA_OPTS% -cp "../lib/*;../*;" %MAIN_CLASS% %MAIN_OPTS% 
--- zbus-dist/zbus.bat
+++ zbus-dist/zbus.bat
@@ -2,4 +2,4 @@ REM SET JAVA_HOME=%JAVA_HOME%\bin\
 SET JAVA_OPTS=-Dfile.encoding=UTF-8 -server -Xms1024m -Xmx4096m -XX:+UseParallelGC
 SET MAIN_CLASS=org.zbus.server.ZbusServer
 SET MAIN_OPTS=-p 15555 -w 3000 -track 127.0.0.1:16666;127.0.0.1:16667 
-START /B java %JAVA_OPTS% -cp "../lib/*;../lib/logging/*;../*;" %MAIN_CLASS% %MAIN_OPTS% 
+START /B java %JAVA_OPTS% -cp "./lib/*;./*;" %MAIN_CLASS% %MAIN_OPTS% 
