213d4774fbd1fefdb6a81842839277494db830fa
少帮主 <44194462@qq.com>
2015-02-10 14:38:25 +0800

    separate api and proxy to different projects

--- zbus-dist/ha/track.sh
+++
@@ -1,6 +0,0 @@
-#/usr/bin
-JAVA_HOME=/apps/jdk7
-JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms1024m -Xmx4096m -XX:+UseParallelGC"
-MAIN_CLASS=org.zbus.server.TrackServer
-MAIN_OPTS="-p 16666" 
-nohup $JAVA_HOME/bin/java $JAVA_OPTS -cp "../lib/*:../*" $MAIN_CLASS $MAIN_OPTS &
--- zbus-dist/ha/zbus2.sh
+++
@@ -1,6 +0,0 @@
-#/usr/bin
-JAVA_HOME=/apps/jdk7
-JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms1024m -Xmx4096m -XX:+UseParallelGC"
-MAIN_CLASS=org.zbus.server.ZbusServer
-MAIN_OPTS="-p 15556 -track 127.0.0.1:16666;127.0.0.1:16667" 
-nohup $JAVA_HOME/bin/java $JAVA_OPTS -cp "../lib/*:../*" $MAIN_CLASS $MAIN_OPTS &
