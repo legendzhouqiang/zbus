d4efbc693f5cb1ffaebde830737a7e49d5fd26f7
少帮主 <44194462@qq.com>
2015-04-19 19:06:17 +0800

    update dist

--- zbus-dist/zbus.bat
+++ zbus-dist/zbus.bat
@@ -1,8 +1,8 @@
 SET JAVA_HOME=D:\SDK\jdk6_x64
-SET ZBUS_HOME=..
+SET ZBUS_HOME=.
 SET JAVA_OPTS=-Dfile.encoding=UTF-8 -server -Xms1024m -Xmx4096m -XX:+UseParallelGC
 SET MAIN_CLASS=org.zbus.server.ZbusServer
-REM -p 15555 -store dummy|sql|redis  (hsqldb preferred)
+REM -p 15555 -store dummy|sql
 SET MAIN_OPTS=-p 15555 -store dummy -openBrowser true
 SET LIB_OPTS=%ZBUS_HOME%/lib;%ZBUS_HOME%/lib/*;%ZBUS_HOME%/conf;
 START /B %JAVA_HOME%\bin\java %JAVA_OPTS% -cp %LIB_OPTS% %MAIN_CLASS% %MAIN_OPTS% 
--- zbus-dist/zbus.sh
+++ zbus-dist/zbus.sh
@@ -1,6 +1,8 @@
 #/usr/bin
 JAVA_HOME=/apps/jdk7
+ZBUS_HOME=.
 JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms1024m -Xmx4096m -XX:+UseParallelGC"
 MAIN_CLASS=org.zbus.server.ZbusServer
 MAIN_OPTS="-p 15555 -store dummy -track 127.0.0.1:16666;127.0.0.1:16667"
-nohup $JAVA_HOME/bin/java $JAVA_OPTS -cp "./ext:./ext/*:./lib:./lib/*" $MAIN_CLASS $MAIN_OPTS &
+LIB_OPTS="$ZBUS_HOME/lib:$ZBUS_HOME/lib/*:$ZBUS_HOME%/conf"
+nohup $JAVA_HOME/bin/java $JAVA_OPTS -cp $LIB_OPTS $MAIN_CLASS $MAIN_OPTS &
