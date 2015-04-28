0a04e8195b01ec7f79acf166f67297ac10acc0c9
少帮主 <44194462@qq.com>
2015-04-28 14:42:20 +0800

    update release

--- zbus/zbus-dist/ha/track2.sh
+++ zbus/zbus-dist/ha/track2.sh
@@ -2,7 +2,7 @@
 JAVA_HOME=/apps/jdk7
 ZBUS_HOME=..
 JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms64m -Xmx512m -XX:+UseParallelGC"
-MAIN_CLASS=org.zbus.server.TrackServer
+MAIN_CLASS=org.zstacks.zbus.server.TrackServer
 MAIN_OPTS="-p 16667"
 LIB_OPTS="$ZBUS_HOME/lib:$ZBUS_HOME/lib/*:$ZBUS_HOME%/conf"
 nohup $JAVA_HOME/bin/java $JAVA_OPTS -cp $LIB_OPTS $MAIN_CLASS $MAIN_OPTS &
--- zbus/zbus-dist/zbus.bat
+++ zbus/zbus-dist/zbus.bat
@@ -1,7 +1,7 @@
 REM SET JAVA_HOME=D:\SDK\jdk6_x64
 SET ZBUS_HOME=.
 SET JAVA_OPTS=-Dfile.encoding=UTF-8 -server -Xms64m -Xmx4096m -XX:+UseParallelGC
-SET MAIN_CLASS=org.zbus.server.ZbusServer
+SET MAIN_CLASS=org.zstacks.zbus.server.ZbusServer
 REM -p 15555 -store dummy|sql
 SET MAIN_OPTS=-p 15555 -store dummy -openBrowser true -track 127.0.0.1:16666;127.0.0.1:16667 -serviceBase work
 SET LIB_OPTS=%ZBUS_HOME%/lib;%ZBUS_HOME%/lib/*;%ZBUS_HOME%/conf;
