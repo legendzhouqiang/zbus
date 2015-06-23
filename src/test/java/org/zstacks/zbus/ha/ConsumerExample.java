80c90b81a411b99aad0d5a2874b958f46301489e
少帮主 <44194462@qq.com>
2015-06-23 19:28:39 +0800

    ha test update

--- src/test/java/org/zstacks/zbus/ha/ConsumerExample.java
+++ src/test/java/org/zstacks/zbus/ha/ConsumerExample.java
@@ -10,7 +10,7 @@ public class ConsumerExample {
 	public static void main(String[] args) throws Exception{  
 		//1）创建Broker代表
 		HaBrokerConfig config = new HaBrokerConfig();
-		config.setTrackAddrList("127.0.0.1:16666:127.0.0.1:16667");
+		config.setTrackAddrList("127.0.0.1:16666;127.0.0.1:16667");
 		Broker broker = new HaBroker(config);
 		
 		//2) 创建消费者
