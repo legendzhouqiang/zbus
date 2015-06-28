0a1ba617727ff864a5294ff286774f59ddc9f08c
少帮主 <44194462@qq.com>
2015-06-28 08:18:54 +0800

    update doc

--- src/test/java/org/zstacks/zbus/ProducerExample.java
+++ src/test/java/org/zstacks/zbus/ProducerExample.java
@@ -17,12 +17,12 @@ public class ProducerExample {
 		Producer producer = new Producer(broker, "MyMQ");
 		producer.createMQ(); //如果已经确定存在，不需要创建
 		
-		for(int i=0;i<1000000;i++){
+		for(int i=0;i<1;i++){
 			Message msg = new Message(); 
 			msg.setBody("hello world");  
 			Message res = producer.sendSync(msg, 1000);
-			//System.out.println(res); 
-			if(i % 1000 == 0){
+			System.out.println(res); 
+			if((i+1) % 1000 == 0){
 				System.out.println("done: "+ i);
 			}
 		}
