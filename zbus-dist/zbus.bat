1675556210eba3c9a17acd3dcd7a4c683c92278a
少帮主 <44194462@qq.com>
2015-09-07 10:57:25 +0800

    update introduction doc

--- zbus-dist/zbus.bat
+++ zbus-dist/zbus.bat
@@ -1,7 +1,14 @@
 REM SET JAVA_HOME=D:\SDK\jdk6_x64
+
 SET ZBUS_HOME=.
 SET JAVA_OPTS=-server -Xms64m -Xmx1024m -XX:+UseParallelGC
 SET MAIN_CLASS=org.zbus.mq.server.MqServer 
-SET MAIN_OPTS=-h 0.0.0.0 -p 15555 -verbose false -conf zbus.properties -store store -track
+SET MAIN_OPTS=-h 0.0.0.0 -p 15555 -verbose false -store store -track
 SET LIB_OPTS=%ZBUS_HOME%/lib;%ZBUS_HOME%/*;
-"%JAVA_HOME%\bin\java" %JAVA_OPTS% -cp %LIB_OPTS% %MAIN_CLASS% %MAIN_OPTS% 
+
+IF NOT EXIST "%JAVA_HOME%" (
+    SET JAVA=java 
+) ELSE (
+    SET JAVA=%JAVA_HOME%\bin\java
+)
+%JAVA% %JAVA_OPTS% -cp %LIB_OPTS% %MAIN_CLASS% %MAIN_OPTS% 
