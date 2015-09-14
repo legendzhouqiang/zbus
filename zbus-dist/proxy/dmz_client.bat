acd57a602b326273014c434372a56c145fa0a442
少帮主 <44194462@qq.com>
2015-09-14 15:49:17 +0800

    update proxy shell/bat

---
+++ zbus-dist/proxy/dmz_client.bat
@@ -0,0 +1,14 @@
+REM SET JAVA_HOME=D:\SDK\jdk6_x64
+
+SET ZBUS_HOME=..
+SET JAVA_OPTS=-server -Xms64m -Xmx1024m -XX:+UseParallelGC
+SET MAIN_CLASS=org.zbus.proxy.DmzClient
+SET MAIN_OPTS=-dmzDown 127.0.0.1:15557 -dmzNotify 127.0.0.1:15558 -target 127.0.0.1:15555
+SET LIB_OPTS=%ZBUS_HOME%/lib;%ZBUS_HOME%/*;
+
+IF NOT EXIST "%JAVA_HOME%" (
+    SET JAVA=java 
+) ELSE (
+    SET JAVA=%JAVA_HOME%\bin\java
+)
+%JAVA% %JAVA_OPTS% -cp %LIB_OPTS% %MAIN_CLASS% %MAIN_OPTS% 
