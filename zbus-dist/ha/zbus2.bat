671c8eccf3cfb21f6fe5419793469a193c3322df
少帮主 <44194462@qq.com>
2015-09-14 15:39:19 +0800

    Add Disconnected event support

--- zbus-dist/ha/zbus2.bat
+++ zbus-dist/ha/zbus2.bat
@@ -2,6 +2,6 @@ REM SET JAVA_HOME=D:\SDK\jdk6_x64
 SET ZBUS_HOME=..
 SET JAVA_OPTS=-server -Xms64m -Xmx1024m -XX:+UseParallelGC
 SET MAIN_CLASS=org.zbus.mq.server.MqServer 
-SET MAIN_OPTS=-h 0.0.0.0 -p 15556 -verbose false -store store2 -track 127.0.0.1:16666;127.0.0.1:16667
+SET MAIN_OPTS=-h 0.0.0.0 -p 15556 -thrift 0.0.0.0:25556 -verbose false -store store2 -track 127.0.0.1:16666;127.0.0.1:16667
 SET LIB_OPTS=%ZBUS_HOME%/lib;%ZBUS_HOME%/*;%ZBUS_HOME%/ha/*;
 "%JAVA_HOME%\bin\java" %JAVA_OPTS% -cp %LIB_OPTS% %MAIN_CLASS% %MAIN_OPTS% 
