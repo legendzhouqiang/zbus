6d322c29aabc3eebfc18ce6a7aaf77d6fd075b56
少帮主 <44194462@qq.com>
2015-09-29 00:01:01 +0800

    update doc

--- src/test/java/org/zbus/net/MyServerAdaptor.java
+++ src/test/java/org/zbus/net/MyServerAdaptor.java
@@ -1,7 +1,5 @@
 package org.zbus.net;
 
-import java.io.IOException;
-
 import org.zbus.net.core.Dispatcher;
 import org.zbus.net.core.IoAdaptor;
 import org.zbus.net.http.Message;
@@ -11,36 +9,25 @@ import org.zbus.net.http.MessageAdaptor;
 
 public class MyServerAdaptor extends MessageAdaptor{ 
 	public MyServerAdaptor(){  
-		uri("/hello", new MessageProcessor() {
-			
+		uri("/hello", new MessageProcessor() { 
 			@Override
 			public Message process(Message request) { 
 				request.setBody("hello");
 				return request;
 			}
-		});
+		});  
 	} 
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {   
-		final Dispatcher dispatcher = new Dispatcher();  
-		IoAdaptor ioAdaptor = new MyServerAdaptor();
-		
+		final Dispatcher dispatcher = new Dispatcher();   
 		final Server server = new Server(dispatcher);
+		
 		//相同的业务处理逻辑可以便捷的侦听多个地址
+		IoAdaptor ioAdaptor = new MyServerAdaptor();
 		server.registerAdaptor(80, ioAdaptor);
 		server.registerAdaptor(8080, ioAdaptor);
 		
     	server.start(); 
-    	
-    	Runtime.getRuntime().addShutdownHook(new Thread(){
-    		@Override
-    		public void run() {  
-    	    	try {
-    	    		server.close();
-					dispatcher.close();
-				} catch (IOException e) { 
-				}
-    		}
-    	}); 
 	}
 }
