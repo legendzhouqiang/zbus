d9f5e26e4253310c1041457aaa67c0a6d1834e59
rushmore <44194462@qq.com>
2015-10-14 20:55:34 +0800

    update zbus.sh

--- zbus-dist/zbus.sh
+++ zbus-dist/zbus.sh
@@ -1,10 +1,10 @@
 #/usr/bin
 JAVA_HOME=/apps/jdk7
-ZBUS_HOME=.
+ZBUS_HOME=./
 JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms64m -Xmx1024m -XX:+UseParallelGC"
 MAIN_CLASS=org.zbus.mq.server.MqServer
-MAIN_OPTS="-h 0.0.0.0 -p 15555 -verbose false -conf zbus.properties -store store -track"
-LIB_OPTS="$ZBUS_HOME/lib:$ZBUS_HOME/*:$ZBUS_HOME/classes"
+MAIN_OPTS="-p 15555 -verbose false -store store -track"
+LIB_OPTS="$ZBUS_HOME/lib/*:$ZBUS_HOME/classes:$ZBUS_HOME/*"
 nohup $JAVA_HOME/bin/java $JAVA_OPTS -cp $LIB_OPTS $MAIN_CLASS $MAIN_OPTS &
 
 
