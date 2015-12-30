271bcd6dec8af269dc6c47a14855f62035979400
rushmore <44194462@qq.com>
2015-12-30 15:50:46 +0800

    add linux shell for HA

--- zbus-dist/ha/zbus.bat
+++ zbus-dist/ha/zbus.bat
@@ -2,7 +2,7 @@ REM SET JAVA_HOME=D:\SDK\jdk6_x64
 SET ZBUS_HOME=..
 SET JAVA_OPTS=-server -Xms64m -Xmx1024m -XX:+UseParallelGC
 SET MAIN_CLASS=org.zbus.mq.server.MqServer 
-SET MAIN_OPTS=-h 0.0.0.0 -p 15555 -thrift 0.0.0.0:25555 -verbose false -store store -track 127.0.0.1:16666;127.0.0.1:16667
+SET MAIN_OPTS=-h 0.0.0.0 -p 15555 -verbose false -store store -track 127.0.0.1:16666;127.0.0.1:16667
 SET LIB_OPTS=%ZBUS_HOME%/lib;%ZBUS_HOME%/*;%ZBUS_HOME%/ha/*;
 IF NOT EXIST "%JAVA_HOME%" (
     SET JAVA=java
