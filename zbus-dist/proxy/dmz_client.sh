f7903069cbede750a2065837a1bfa915c42c1477
rushmore <44194462@qq.com>
2016-01-05 22:04:37 +0800

    remove proxy and thrift

--- zbus-dist/proxy/dmz_client.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin
-JAVA_HOME=/apps/jdk7
-ZBUS_HOME=..
-JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms64m -Xmx1024m -XX:+UseParallelGC"
-MAIN_CLASS=org.zbus.proxy.DmzClient
-MAIN_OPTS="-dmzDown 127.0.0.1:15557 -dmzNotify 127.0.0.1:15558 -target 127.0.0.1:15555"
-LIB_OPTS="$ZBUS_HOME/lib:$ZBUS_HOME/*"
-nohup $JAVA_HOME/bin/java $JAVA_OPTS -cp $LIB_OPTS $MAIN_CLASS $MAIN_OPTS &
