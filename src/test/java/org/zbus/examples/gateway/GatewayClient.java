daa5f0baee4e3df4c623eaa9128ac9678a1dba4d
rushmore <44194462@qq.com>
2016-01-29 20:55:44 +0800

    GatewayClient change to rpc client

--- src/test/java/org/zbus/examples/gateway/GatewayClient.java
+++ src/test/java/org/zbus/examples/gateway/GatewayClient.java
@@ -1,31 +1,32 @@
 package org.zbus.examples.gateway;
 
-import org.zbus.mq.Protocol;
-import org.zbus.net.core.SelectorGroup;
-import org.zbus.net.http.Message;
-import org.zbus.net.http.MessageClient;
- 
+import org.zbus.broker.Broker;
+import org.zbus.broker.BrokerConfig;
+import org.zbus.broker.SingleBroker;
+import org.zbus.examples.rpc_biz.Interface;
+import org.zbus.net.http.Message.MessageInvoker;
+import org.zbus.rpc.RpcFactory;
+import org.zbus.rpc.mq.MqInvoker;
 
 public class GatewayClient {
 
-	public static void main(String[] args) throws Exception { 
-		SelectorGroup group = new SelectorGroup();
-		MessageClient client = new MessageClient("127.0.0.1:15555", group);
-		
-		Message req = new Message();
-		req.setMq("Gateway");
-		req.setCmd(Protocol.Produce);
-		req.setAck(false);
-		req.setBody("test");
-		
-		
-		Message res = client.invokeSync(req);
+	public static void main(String[] args) throws Exception {
+		// 1)创建Broker代表（可用高可用替代）
+		BrokerConfig config = new BrokerConfig();
+		config.setServerAddress("127.0.0.1:8080");
+		Broker broker = new SingleBroker(config);
+
+		// 2)创建基于MQ的Invoker以及Rpc工厂，指定RPC采用的MQ为MyRpc
+		MessageInvoker invoker = new MqInvoker(broker, "MyGateway");
+		RpcFactory factory = new RpcFactory(invoker);
+
+		// 3) 动态代理出实现类
+		Interface hello = factory.getService(Interface.class);
+ 
+		Object res = hello.echo("test");
 		System.out.println(res);
 		
-		client.close();
-		group.close(); 
-		
-		
+		broker.close(); 
 	}
 
 }
