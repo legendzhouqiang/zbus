92f11a90850d4c6d72db7725f6fe37d5a95518e4
rushmore <44194462@qq.com>
2016-03-18 08:55:34 +0800

    DefaultAuth updated for null bug

--- src/main/java/org/zbus/mq/server/auth/DefaultAuth.java
+++ src/main/java/org/zbus/mq/server/auth/DefaultAuth.java
@@ -8,9 +8,12 @@ public class DefaultAuth implements Auth{
 	@Override
 	public boolean auth(String appid, String token) {
 		if(token == null) token = ""; //null assumed to be empty
-		return accessToken.equals(token);
+		return token.equals(accessToken);
 	}
 	public void setAccessToken(String accessToken) {
+		if(accessToken == null){ //null assumed to be empty
+			accessToken = "";
+		}
 		this.accessToken = accessToken;
 	} 
 }
