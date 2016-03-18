8c3132da11bde262426296b48fcfb7ba970c9f48
rushmore <44194462@qq.com>
2016-03-18 12:24:51 +0800

    Fix DefaultAuth null bug

--- src/main/java/org/zbus/mq/server/auth/DefaultAuth.java
+++ src/main/java/org/zbus/mq/server/auth/DefaultAuth.java
@@ -3,12 +3,15 @@ package org.zbus.mq.server.auth;
 public class DefaultAuth implements Auth{
 	private String accessToken = "";
 	public DefaultAuth(String accessToken){
+		if(accessToken == null){
+			accessToken = "";
+		}
 		this.accessToken = accessToken;
 	}
 	@Override
 	public boolean auth(String appid, String token) {
 		if(token == null) token = ""; //null assumed to be empty
-		return token.equals(accessToken);
+		return accessToken.equals(token);
 	}
 	public void setAccessToken(String accessToken) {
 		if(accessToken == null){ //null assumed to be empty
