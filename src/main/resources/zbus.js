42b9ff1435ea93592ef399ab287733579c1d956b
rushmore <44194462@qq.com>
2017-02-01 23:50:16 +1300

    udpate zbus.js websocket

--- src/main/resources/zbus.js
+++ src/main/resources/zbus.js
@@ -172,49 +172,109 @@ function underscore2camel(key){
 	return key.replace(/_([a-z])/g, function (g) { return g[1].toUpperCase(); });
 }
 
+/** 
+ * msg.encoding = "utf8"; //encoding on message body
+ * msg.body can be 
+ * 1) ArrayBuffer, Uint8Array, Init8Array to binary data
+ * 2) string value
+ * 3) otherwise, JSON converted inside
+ * 
+ * @param msg
+ * @returns ArrayBuffer
+ */
 function httpDump(msg){
-	var lines = new Array();
+	var headers = "";
+	var encoding = msg.encoding;
+	if(!encoding) encoding = "utf8";
+	var encoder = new TextEncoder(encoding);
 	
-	var body = msg.body;
-	var bodyString = "\r\n";
-	var bodyLen = 0;
-	if(body){ 
-		if(body instanceof Int16Array){
-			body = buffer2String(body);
-		} 
-		bodyString += body;
-		bodyLen = body.length;
-	}
-	msg['content-length'] = bodyLen;
+	var body = msg.body;  
+	var contentType = "application/octet-stream";
+	if (body) { 
+		if(body instanceof ArrayBuffer){
+			body = new Uint8Array(body);
+		} else if(body instanceof Uint8Array || body instanceof Int8Array){
+			//no need to handel
+		} else { 
+			if (typeof body != 'string') {
+				body = JSON.stringify(body);
+				contentType = "application/json";
+			} else {
+				contentType = "text/html";
+			} 
+			body = encoder.encode(body); 
+		}  
+	} 
+	msg["content-length"] = body.byteLength;
+	msg["content-type"] = contentType;
 	
 	var nonHeaders = {'status': true, 'method': true, 'url': true, 'body': true}
-	var meta = ""
+	var line = ""
 	if(msg.status){
 		var desc = HttpStatus[msg.status]; 
     	if(!desc) desc = "Unknown Status";
-		meta = "HTTP/1.1 {0} {1}".format(msg.status, desc); 
+		line = "HTTP/1.1 {0} {1}\r\n".format(msg.status, desc); 
 	} else {
 		var method = msg.method;
 		if(!method) method = "GET";
 		var url = msg.url;
 		if(!url) url = "/";
-		meta = "{0} {1} HTTP/1.1".format(method, url);  
+		line = "{0} {1} HTTP/1.1\r\n".format(method, url);  
 	}
-	lines.push(meta);
-	for(var key in msg){
-		if(key in nonHeaders) continue; 
-		//replace camel style to underscore style
-		key = camel2underscore(key);
-		lines.push("{0}: {1}".format(key, msg[key])); 
-	}   
 	
-	lines.push(bodyString);  
-	return lines.join("\r\n");
+	
+	headers += line; 
+
+
+	for(var key in msg){
+		if(key in nonHeaders) continue;  
+		line = "{0}: {1}\r\n".format(camel2underscore(key), msg[key]);
+		headers += line;
+	}
+	headers += "\r\n";
+	delete msg["content-length"]; //clear
+	delete msg["content-type"]
+     
+	var headerBuffer = encoder.encode(headers); 
+	var headerLen = headerBuffer.byteLength;
+	//merge header and body
+	var buffer = new ArrayBuffer(headerBuffer.byteLength + body.byteLength);
+	var view = new Uint8Array(buffer); 
+	for(var i=0;i<headerBuffer.byteLength;i++){
+		view[i] = headerBuffer[i];
+	}
+	
+	for(var i=0;i<body.byteLength;i++){
+		view[headerLen+i] = body[i];
+	}
+	return buffer; 
 };
 
-
-//TODO handle when body is binary
-function httpParse(str){
+ 
+function httpParse(data){
+	if(typeof data == "string"){
+		data = new TextEncoder("utf8").encode();
+	} else if (data instanceof Uint8Array || data instanceof Int8Array){
+		//ignore
+	} else if (data instanceof ArrayBuffer) {
+		data = new Uint8Array(data);
+	} else {
+		//not support type
+		return null;
+	}
+	var i = 0, pos = -1; 
+	var CR = 13, NL = 10;
+	while(i+3<data.byteLength){
+		if(data[i]==CR && data[i+1]==NL && data[i+2]==CR && data[i+3]==NL){
+			pos = i; 
+			break;
+		}
+		i++;
+	} 
+	if(pos == -1) return null;
+	
+	var str = new TextDecoder("utf8").decode(data.slice(0, pos));
+	
 	var blocks = str.split("\r\n");
 	var lines = [];
 	for(var i in blocks){
@@ -222,8 +282,7 @@ function httpParse(str){
 		if(line == '') continue;
 		lines.push(line);
 	}
-	var lenKey = "content-length";
-	var lenVal = 0;
+	
 	var msg = {};
 	//parse first line 
 	var bb = lines[0].split(" "); 
@@ -233,27 +292,44 @@ function httpParse(str){
 		msg.method = bb[0];
 		msg.url = bb[1];
 	}
+	var typeKey = "content-type";
+	var typeVal = "text/html";
+	var lenKey = "content-length";
+	var lenVal = 0;
 	
 	for(var i=1;i<lines.length;i++){
-		var line = lines[i];
-		if(i == lines.length-1){
-			if(lenVal > 0){
-				msg.body = line;
-				continue;
-			}
-		}
-		
+		var line = lines[i]; 
 		var p = line.indexOf(":");
 		if(p == -1) continue;
 		var key = line.substring(0, p).trim().toLowerCase();
 		var val = line.substring(p+1).trim(); 
 		if(key == lenKey){
-			lenVal = val;
+			lenVal = parseInt(val);
 			continue;
 		} 
+		if(key == typeKey){
+			typeVal = val;
+			continue;
+		}
+		
 		key = underscore2camel(key);
 		msg[key] = val;
 	} 
+	if(pos+4+lenVal > data.byteLength){
+		return null;
+	}
+	var encoding = msg.encoding;
+	if(!encoding) encoding = "utf8";
+	var decoder = new TextDecoder(encoding);
+	var bodyData = data.slice(pos+4);
+	if(typeVal == "text/html"){
+		msg.body = decoder.decode(bodyData);
+	} else if(typeVal == "application/json"){
+		msg.body = JSON.parse(decoder.decode(bodyData));
+	} else {
+		msg.body = bodyData.buffer;
+	} 
+	
 	return msg;	 
 }
 
