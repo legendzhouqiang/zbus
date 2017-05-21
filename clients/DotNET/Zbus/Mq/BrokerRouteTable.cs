ec1d7da601d0cf3d255c8649e26942141ff1003c
rushmore <44194462@qq.com>
2017-05-21 21:06:29 +1200

    Zbus.C# broker updated

--- clients/DotNET/Zbus/Mq/BrokerRouteTable.cs
+++ clients/DotNET/Zbus/Mq/BrokerRouteTable.cs
@@ -17,19 +17,63 @@ namespace Zbus.Mq
         {
             VotesTable = new ConcurrentDictionary<ServerAddress, HashSet<ServerAddress>>();
             TopicTable = new ConcurrentDictionary<string, IList<TopicInfo>>();
-            ServerTable = new ConcurrentDictionary<ServerAddress, ServerInfo>();
+            ServerTable = new ConcurrentDictionary<ServerAddress, ServerInfo>(); 
+        }
+
+        public bool CanRemove(ISet<ServerAddress> votedTrackerSet)
+        {
+            return votedTrackerSet.Count < 1;
         }
 
         public IList<ServerAddress> UpdateVotes(TrackerInfo trackerInfo)
         {
-            Console.WriteLine("TrackerUpdated:" + JsonKit.SerializeObject(trackerInfo));
-            return new List<ServerAddress>();
+            ServerAddress trackerAddress = trackerInfo.ServerAddress;
+            var localVotesTable = new ConcurrentDictionary<ServerAddress, HashSet<ServerAddress>>(VotesTable);
+            var trackedServerSet = new HashSet<ServerAddress>(trackerInfo.TrackedServerList);
+            foreach(var serverAddress in trackedServerSet)
+            {
+                var votedTracker = new HashSet<ServerAddress>();
+                if (localVotesTable.ContainsKey(serverAddress))
+                {
+                    votedTracker = localVotesTable[serverAddress];
+                }
+                votedTracker.Add(trackerAddress);
+            } 
+            var toRemove = new List<ServerAddress>();
+            foreach(var kv in localVotesTable)
+            {
+                ServerAddress serverAddress = kv.Key;
+                HashSet<ServerAddress> votedTrackers = kv.Value;
+                if(votedTrackers.Contains(trackerAddress) && !trackedServerSet.Contains(serverAddress))
+                {
+                    votedTrackers.Remove(trackerAddress);
+                }
+                if (CanRemove(votedTrackers))
+                {
+                    toRemove.Add(serverAddress);
+                }
+            }
+            this.VotesTable = localVotesTable;
+            if(toRemove.Count == 0)
+            {
+                return toRemove;
+            }
+
+            foreach(ServerAddress serverAddress in toRemove)
+            {
+                if (ServerTable.ContainsKey(serverAddress))
+                {
+                    ServerTable.Remove(serverAddress);
+                } 
+            }
+            this.RebuildTopicTable(); 
+            return toRemove;
         }
 
         public void UpdateServer(ServerInfo serverInfo)
         {
-            ServerTable[serverInfo.ServerAddress] = serverInfo; 
-            Console.WriteLine("Updated:" + JsonKit.SerializeObject(serverInfo));
+            ServerTable[serverInfo.ServerAddress] = serverInfo;
+            RebuildTopicTable();
         }
 
         public void RemoveServer(ServerAddress serverAddress)
@@ -38,8 +82,32 @@ namespace Zbus.Mq
             {
                 ServerTable.Remove(serverAddress);
             }
-
-            Console.WriteLine("Removed:" + JsonKit.SerializeObject(serverAddress));
+            RebuildTopicTable(); 
         } 
+
+        private void RebuildTopicTable()
+        {
+            var localTopicTable = new ConcurrentDictionary<string, IList<TopicInfo>>();
+            foreach(var kv in ServerTable)
+            {
+                ServerInfo serverInfo = kv.Value;
+                foreach(var tkv in serverInfo.TopicTable)
+                {
+                    TopicInfo topicInfo = tkv.Value;
+                    IList<TopicInfo> topicServerList = null;
+                    if (!localTopicTable.ContainsKey(topicInfo.TopicName))
+                    {
+                        topicServerList = new List<TopicInfo>();
+                        localTopicTable[topicInfo.TopicName] = topicServerList;
+                    } 
+                    else
+                    {
+                        topicServerList = localTopicTable[topicInfo.TopicName];
+                    }
+                    topicServerList.Add(topicInfo);
+                }
+            }
+            this.TopicTable = localTopicTable;
+        }
     }
 }
