ad67db094be46b47cbcb18683fbe0eccedcd1a3b
rushmore <44194462@qq.com>
2017-08-28 20:31:59 +1200

    TcpProxy support added

--- src/main/java/io/zbus/proxy/tcp/TcpProxy.java
+++ src/main/java/io/zbus/proxy/tcp/TcpProxy.java
@@ -1,10 +1,87 @@
 package io.zbus.proxy.tcp;
 
-public class TcpProxy {
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import io.zbus.kit.ConfigKit;
+import io.zbus.kit.logging.Logger;
+import io.zbus.kit.logging.LoggerFactory;
+import io.zbus.transport.ServerAdaptor;
+import io.zbus.transport.Session;
+import io.zbus.transport.tcp.TcpServer;
 
+public class TcpProxy extends ServerAdaptor implements Closeable {
+	protected static final Logger log = LoggerFactory.getLogger(TcpProxy.class);
+	private int port; 
+	private String targetHost;
+	private int targetPort = 80;
+	private TcpServer server;   
+	
+	public TcpProxy(int port, String target) { 
+		this.port = port;
+		String[] bb = target.split("[:]");
+		if(bb.length > 0){
+			this.targetHost = bb[0].trim();
+		}
+		if(bb.length > 1){
+			this.targetPort = Integer.valueOf(bb[1].trim());
+		} 
 	}
-
+	
+	@Override
+	public void sessionCreated(Session sess) throws IOException { 
+		super.sessionCreated(sess); //add to session table
+		
+		ProxyClient client = new ProxyClient(sess, targetHost, targetPort, server.getEventLoop()); 
+		sess.attr("downClient", client);
+	} 
+	 
+	
+	protected void cleanSession(Session sess){
+		try { 
+			ProxyClient client = sess.attr("downClient");
+			if(client != null){
+				client.close();
+			}
+			super.cleanSession(sess);
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+	
+	@Override
+	public void onMessage(Object msg, Session sess) throws IOException {
+		Session down = sess.attr("down");
+		if(down == null){
+			Queue<Object> delayed = sess.attr("delayed");
+			if(delayed == null){
+				delayed = new ConcurrentLinkedQueue<Object>();
+			}
+			sess.attr("delayed", delayed);
+			delayed.add(msg);
+		} else {
+			down.write(msg);
+		}
+	} 
+	
+	@Override
+	public void close() throws IOException { 
+		server.close(); 
+	}
+	
+	public void start(){
+		server = new TcpServer();   
+		server.start(port, this);
+	}
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		int port = ConfigKit.option(args, "-p", 80);
+		String target = ConfigKit.option(args, "-t", "127.0.0.1:15555"); 
+		
+		TcpProxy proxy = new TcpProxy(port, target);
+		proxy.start();
+	} 
 }
