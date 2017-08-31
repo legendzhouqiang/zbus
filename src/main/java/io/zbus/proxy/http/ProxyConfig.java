4850f419c62016b4e7ce15d20ff6dce1587d3b7f
rushmore <44194462@qq.com>
2017-08-31 22:57:17 +1200

    refine http proxy

--- src/main/java/io/zbus/proxy/http/ProxyConfig.java
+++ src/main/java/io/zbus/proxy/http/ProxyConfig.java
@@ -1,26 +1,38 @@
 package io.zbus.proxy.http;
 
+import static io.zbus.kit.ConfigKit.valueOf;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import io.zbus.kit.FileKit;
 import io.zbus.mq.Broker;
-import io.zbus.mq.BrokerConfig;
 
-public class ProxyConfig {
-	public BrokerConfig brokerConfig;
-	public Broker broker;
-	public String entry;  //zbus's Topic entry
-	public String target; //Target http server 
+public class ProxyConfig { 
+	public Broker broker; 
+	public String brokerAddress;
 	public int connectionCount = 4; //Number of connections to zbus broker per consumer 
+	public Map<String, ProxyEntry> entryTable = new HashMap<String, ProxyEntry>(); 
 
-	public BrokerConfig getBrokerConfig() {
-		return brokerConfig;
-	}
-
-	public String getEntry() {
-		return entry;
-	}
-
-	public String getTarget() {
-		return target;
-	}
+	public static class ProxyEntry {
+		public String entry;
+		public List<String> targetList = new ArrayList<String>();
+	} 
 
 	public Broker getBroker() {
 		return broker;
@@ -28,25 +40,80 @@ public class ProxyConfig {
 
 	public int getConnectionCount() {
 		return connectionCount;
-	}
-
-	public void setBrokerConfig(BrokerConfig brokerConfig) {
-		this.brokerConfig = brokerConfig;
-	}
-
-	public void setEntry(String entry) {
-		this.entry = entry;
-	}
-
-	public void setTarget(String target) {
-		this.target = target;
-	}
-
+	} 
+ 
 	public void setBroker(Broker broker) {
 		this.broker = broker;
 	}
 
 	public void setConnectionCount(int connectionCount) {
 		this.connectionCount = connectionCount;
+	} 
+	
+	public String getBrokerAddress() {
+		return brokerAddress;
 	}
+
+	public void setBrokerAddress(String brokerAddress) {
+		this.brokerAddress = brokerAddress;
+	} 
+
+	public Map<String, ProxyEntry> getEntryTable() {
+		return entryTable;
+	}
+
+	public void setEntryTable(Map<String, ProxyEntry> entryTable) {
+		this.entryTable = entryTable;
+	}
+
+	public void loadFromXml(InputStream stream) throws Exception{
+		XPath xpath = XPathFactory.newInstance().newXPath();     
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		InputSource source = new InputSource(stream); 
+		Document doc = db.parse(source); 
+		
+		this.brokerAddress = valueOf(xpath.evaluate("/zbus/httpProxy/@zbus", doc), "localhost:15555");  
+		this.connectionCount = valueOf(xpath.evaluate("/zbus/httpProxy/@connectionCount", doc), 4);   
+		 
+		NodeList entryList = (NodeList) xpath.compile("/zbus/httpProxy/*").evaluate(doc, XPathConstants.NODESET);
+		if(entryList != null && entryList.getLength()> 0){ 
+			for (int i = 0; i < entryList.getLength(); i++) {
+			    Node node = entryList.item(i);    
+			    ProxyEntry entry = new ProxyEntry();
+			    String entryName = valueOf(xpath.evaluate("@entry", node), ""); 
+			    if (entryName.equals("")) continue;
+			    entry.entry = entryName;
+			    
+			    NodeList targetList = (NodeList) xpath.compile("./*").evaluate(node, XPathConstants.NODESET);
+			    for (int j = 0; j < targetList.getLength(); j++) {
+				    Node targetNode = targetList.item(j);    
+				    String target = targetNode.getFirstChild().getNodeValue();
+				    entry.targetList.add(target);
+			    } 
+			    
+			    this.entryTable.put(entryName, entry);
+			}
+		}   
+	}
+	
+	public void loadFromXml(String configFile) { 
+		InputStream stream = FileKit.inputStream(configFile);
+		if(stream == null){
+			throw new IllegalArgumentException(configFile + " not found");
+		}
+		try { 
+			loadFromXml(stream); 
+		} catch (Exception e) { 
+			throw new IllegalArgumentException(configFile + " load error", e);
+		} finally {
+			if(stream != null){
+				try {
+					stream.close();
+				} catch (IOException e) {
+					//ignore
+				}
+			}
+		}
+	} 
 }
---
+++ src/main/java/io/zbus/proxy/http/ProxyHandler.java
@@ -0,0 +1,215 @@
+package io.zbus.proxy.http;
+
+import java.io.Closeable;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import io.zbus.kit.logging.Logger;
+import io.zbus.kit.logging.LoggerFactory;
+import io.zbus.mq.Broker;
+import io.zbus.mq.Consumer;
+import io.zbus.mq.ConsumerConfig;
+import io.zbus.mq.Message;
+import io.zbus.mq.MessageHandler;
+import io.zbus.mq.MqClient;
+import io.zbus.mq.Protocol;
+import io.zbus.transport.Client.ConnectedHandler;
+import io.zbus.transport.Client.DisconnectedHandler;
+import io.zbus.transport.Session;
+ 
+
+public class ProxyHandler implements MessageHandler, Closeable {
+	protected static final Logger log = LoggerFactory.getLogger(ProxyHandler.class);
+	
+	private final String entry;
+	private final String prefix;
+	private final String targetServer;
+	private final String targetUrl;
+	private int connectionCount;
+	private Broker broker; 
+	private Consumer consumer; 
+	private List<HttpClient> targetClients;
+	private int currentClient = 0;  
+	
+	public ProxyHandler(String entry, String target, Broker broker, int connectionCount) {
+		this.connectionCount = connectionCount;
+		this.entry = entry;
+		this.prefix = "/" + entry;
+		this.broker = broker;
+		 
+		if(target.startsWith("http://")){
+			target  = target.substring("http://".length());
+		}
+		String[] bb = target.split("[//]",2);
+		this.targetServer = bb[0].trim();
+		String url = "";
+		if(bb.length>1){
+			url = bb[1].trim();
+		} 
+		this.targetUrl = url; 
+		
+		targetClients = new ArrayList<HttpClient>();
+		for(int i=0;i<this.connectionCount;i++){
+			HttpClient client = new HttpClient();
+			try {
+				client.connect();
+			} catch (IOException e) {
+				log.error(e.getMessage(), e);
+			}
+			targetClients.add(client);
+		}
+	}
+
+	public synchronized void start() throws IOException {
+		if (consumer != null) return;
+		ConsumerConfig config = new ConsumerConfig(this.broker);
+		config.setTopic(entry);
+		config.setConnectionCount(this.connectionCount);
+		config.setTopicMask(Protocol.MASK_MEMORY);
+		config.setMaxInFlightMessage(1); //run each time
+		consumer = new Consumer(config);
+		consumer.setMessageHandler(this); 
+		consumer.start();
+	}
+	
+
+	@Override
+	public void handle(Message msg, MqClient client) throws IOException {  
+		String url = msg.getUrl();
+
+		if (url == null) {
+			log.error("missing url");
+			return;
+		}
+		if (url.startsWith(prefix)) {
+			url = url.substring(prefix.length());
+			if (!url.startsWith("/")) {
+				url = "/" + url;
+			}
+		}  
+		url = targetUrl + url;
+		if (!url.startsWith("/")) {
+			url = "/" + url;
+		}
+		
+		msg.setUrl(url);
+		
+		Message res = null;
+		try { 
+			currentClient = (currentClient+1)%connectionCount;
+			HttpClient targetClient = targetClients.get(currentClient);
+			targetClient.sendMessage(client, msg); 
+		} catch (Exception e) {
+			res = new Message();
+			if (e instanceof FileNotFoundException) {
+				res.setStatus(404);
+				res.setBody(e.getMessage() + " Not Found");
+			} else {
+				res.setStatus(500);
+				String error = String.format("Target(%s/%s) invoke error, reason: %s", targetServer, targetUrl, e.toString());
+				res.setBody(error);
+			}
+		}   
+	}
+	 
+	@Override
+	public void close() throws IOException {
+		if (consumer != null) {
+			consumer.close();
+			consumer = null;
+		} 
+	}   
+    
+	class HttpClient {
+		MqClient client; 
+		Queue<Context> requests = new ConcurrentLinkedQueue<Context>();
+		Map<String, Context> requestTable = new ConcurrentHashMap<String, Context>();
+		
+		HttpClient() { 
+			client = new MqClient(targetServer, broker.getEventLoop());
+			client.onDisconnected(new DisconnectedHandler() { 
+				@Override
+				public void onDisconnected() throws IOException { 
+				}
+			});
+			
+			client.onConnected(new ConnectedHandler() {
+				
+				@Override
+				public void onConnected() throws IOException {   
+				}
+			}); 
+			
+			client.onMessage(new io.zbus.transport.MessageHandler<Message>() { 
+				@Override
+				public void handle(Message res, Session session) throws IOException {
+					String msgId = res.getId(); 
+					Context ctx = null;
+					if(msgId != null){
+						ctx = requestTable.remove(msgId);
+					}  
+					if(ctx != null){
+						requests.remove(ctx);
+					} else { //MsgId not set
+						ctx = requests.poll();
+						if(ctx != null) {
+							Iterator<Entry<String, Context>> iter = requestTable.entrySet().iterator();
+							while(iter.hasNext()){
+								Entry<String, Context> e = iter.next();
+								if(e.getValue() == ctx){
+									iter.remove();
+									break;
+								}
+							}
+						}
+					}
+					
+					if(ctx == null){ 
+						return; //ignore
+					}
+					
+					res.setId(ctx.msgId);
+					res.setTopic(ctx.topic);
+					res.setReceiver(ctx.sender); 
+					try {
+						ctx.senderClient.route(res);
+					} catch (IOException e) {
+						log.error(e.getMessage(), e);
+					}
+				}
+			});
+		}
+		
+		void sendMessage(MqClient senderClient, Message msg) throws IOException, InterruptedException{
+			Context ctx = new Context();
+			ctx.msgId = msg.getId();
+			ctx.topic = msg.getTopic();
+			ctx.sender = msg.getSender();
+			ctx.senderClient = senderClient;
+			
+			requests.add(ctx);
+			requestTable.put(ctx.msgId, ctx);
+			
+			client.sendMessage(msg); 
+		}
+		
+		void connect() throws IOException{
+			client.connectAsync();  
+		}
+		
+		class Context{
+			String topic;
+			String msgId;
+			String sender;
+			MqClient senderClient;
+		}
+	} 
+}
