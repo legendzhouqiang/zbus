0b53bdcd23122834b3c276a9c47bde80ad35ebbb
rushmore <44194462@qq.com>
2017-09-05 23:25:16 +1200

    refine http proxy for auth

--- src/test/java/io/zbus/examples/proxy/HttpProxyExample.java
+++ src/test/java/io/zbus/examples/proxy/HttpProxyExample.java
@@ -1,20 +1,11 @@
 package io.zbus.examples.proxy;
 
-import io.zbus.kit.ConfigKit;
 import io.zbus.proxy.http.HttpProxy;
-import io.zbus.proxy.http.ProxyConfig;
 
 public class HttpProxyExample {
 
 	public static void main(String[] args) throws Exception {
-		//HttpProxy.main(args);
-		String configFile = ConfigKit.option(args, "-conf", "conf/http_proxy.xml"); 
-		ProxyConfig config = new ProxyConfig();
-		config.loadFromXml(configFile);
-		  
-		HttpProxy proxy = new HttpProxy(config);
-		proxy.start();
-		proxy.close();
+		HttpProxy.main(args); 
 	}
 
 }
