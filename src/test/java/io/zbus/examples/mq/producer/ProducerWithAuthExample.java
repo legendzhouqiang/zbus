5a45055273e835568ec38dcc2829bbaf24547abe
rushmore <44194462@qq.com>
2017-09-25 18:59:19 +1300

    update examples

--- src/test/java/io/zbus/examples/mq/producer/ProducerWithAuthExample.java
+++ src/test/java/io/zbus/examples/mq/producer/ProducerWithAuthExample.java
@@ -2,18 +2,22 @@ package io.zbus.examples.mq.producer;
 
 import io.zbus.mq.Broker;
 import io.zbus.mq.Message;
-import io.zbus.mq.Producer; 
+import io.zbus.mq.Producer;
+import io.zbus.mq.ProducerConfig; 
 
 public class ProducerWithAuthExample { 
 	public static void main(String[] args) throws Exception { 
 		String token = "MyTopic_token";
 		Broker broker = new Broker("localhost:15555", token); 
 		  
-		Producer p = new Producer(broker);
+		ProducerConfig config = new ProducerConfig();
+		config.setBroker(broker);
+		config.setToken(token);
+		
+		Producer p = new Producer(config);
 		p.declareTopic("MyTopic"); 
 		 
-		Message msg = new Message();
-		msg.setToken(token);
+		Message msg = new Message(); 
 		msg.setTopic("MyTopic");
 		//msg.setTag("oo.account.pp");
 		msg.setBody("hello " + System.currentTimeMillis()); 
