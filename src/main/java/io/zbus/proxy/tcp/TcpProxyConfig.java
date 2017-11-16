2eedd5001ee3b783cdb4e821c3d1421b589824f7
rushmore <44194462@qq.com>
2017-11-16 17:19:50 +0800

    TcpProxy config support multiple entries

--- src/main/java/io/zbus/proxy/tcp/TcpProxyConfig.java
+++ src/main/java/io/zbus/proxy/tcp/TcpProxyConfig.java
@@ -2,63 +2,91 @@ package io.zbus.proxy.tcp;
 
 import static io.zbus.kit.ConfigKit.valueOf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import io.zbus.kit.ConfigKit.XmlConfig;
 import io.zbus.kit.StrKit;
 
-public class TcpProxyConfig extends XmlConfig{
-	private String proxyHost = "0.0.0.0"; 
-	private int proxyPort = 80; 
-	private String targetAddress;      //IP:Port
-	private int connectTimeout = 3000; //milliseconds
-	private int idleTimeout = 60000;   //milliseconds 
+public class TcpProxyConfig extends XmlConfig{ 
+	
+	private List<ProxyEntry> entries = new ArrayList<ProxyEntry>(); 
 	
 	public void loadFromXml(Document doc) throws Exception{
 		XPath xpath = XPathFactory.newInstance().newXPath();    
 		  
-		String server = xpath.evaluate("/zbus/tcpProxy/server", doc); 
-		this.targetAddress = xpath.evaluate("/zbus/tcpProxy/target", doc); 
-	    this.connectTimeout = valueOf(xpath.evaluate("/zbus/tcpProxy/connectTimeout", doc), 3000);
-	    this.idleTimeout = valueOf(xpath.evaluate("/zbus/tcpProxy/idleTimeout", doc), 60000); 
-	   
-	    Object[] hp = StrKit.hostPort(server);
-	    this.proxyHost = (String)hp[0];
-	    this.proxyPort = (Integer)hp[1];  
+		NodeList entryList = (NodeList) xpath.compile("/zbus/tcpProxy/*").evaluate(doc, XPathConstants.NODESET);
+		if(entryList != null && entryList.getLength()> 0){ 
+			for (int i = 0; i < entryList.getLength(); i++) {
+			    Node node = entryList.item(i);    
+			    ProxyEntry entry = new ProxyEntry();
+			    
+			    String server = xpath.evaluate("./server", node);  
+				
+				entry.targetAddress = xpath.evaluate("./target", node); 
+				entry.connectTimeout = valueOf(xpath.evaluate("./connectTimeout", node), 3000);
+				entry.idleTimeout = valueOf(xpath.evaluate("./idleTimeout", node), 60000); 
+			   
+			    Object[] hp = StrKit.hostPort(server);
+			    entry.proxyHost = (String)hp[0];
+			    entry.proxyPort = (Integer)hp[1];  
+			    
+			    entries.add(entry);
+			}
+		}    
 	} 
 	
-	public String getProxyHost() {
-		return proxyHost;
+	public List<ProxyEntry> getEntries() {
+		return entries;
 	}
-	public void setProxyHost(String proxyHost) {
-		this.proxyHost = proxyHost;
-	}
-	public int getProxyPort() {
-		return proxyPort;
-	}
-	public void setProxyPort(int proxyPort) {
-		this.proxyPort = proxyPort;
-	}
-	public String getTargetAddress() {
-		return targetAddress;
-	}
-	public void setTargetAddress(String targetAddress) {
-		this.targetAddress = targetAddress;
-	}
-	public int getConnectTimeout() {
-		return connectTimeout;
-	}
-	public void setConnectTimeout(int connectTimeout) {
-		this.connectTimeout = connectTimeout;
-	}
-	public int getIdleTimeout() {
-		return idleTimeout;
-	}
-	public void setIdleTimeout(int idleTimeout) {
-		this.idleTimeout = idleTimeout;
-	}  
-
+	
+	public void setEntries(List<ProxyEntry> entries) {
+		this.entries = entries;
+	} 
+	
+	public static class ProxyEntry{
+		public String proxyHost = "0.0.0.0"; 
+		public int proxyPort = 80; 
+		public String targetAddress;      //IP:Port
+		public int connectTimeout = 3000; //milliseconds
+		public int idleTimeout = 60000;   //milliseconds 
+		public String getProxyHost() {
+			return proxyHost;
+		}
+		public void setProxyHost(String proxyHost) {
+			this.proxyHost = proxyHost;
+		}
+		public int getProxyPort() {
+			return proxyPort;
+		}
+		public void setProxyPort(int proxyPort) {
+			this.proxyPort = proxyPort;
+		}
+		public String getTargetAddress() {
+			return targetAddress;
+		}
+		public void setTargetAddress(String targetAddress) {
+			this.targetAddress = targetAddress;
+		}
+		public int getConnectTimeout() {
+			return connectTimeout;
+		}
+		public void setConnectTimeout(int connectTimeout) {
+			this.connectTimeout = connectTimeout;
+		}
+		public int getIdleTimeout() {
+			return idleTimeout;
+		}
+		public void setIdleTimeout(int idleTimeout) {
+			this.idleTimeout = idleTimeout;
+		}  
+	} 
 }
