d34ea5438bdb68f81d9595f2117ee651c08f6345
rushmore <44194462@qq.com>
2018-03-06 14:59:43 +0800

    update

--- src/main/java/io/zbus/net/http/HttpInvoker.java
+++ src/main/java/io/zbus/net/http/HttpInvoker.java
@@ -5,11 +5,10 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.alibaba.fastjson.JSON;
 
+import io.zbus.kit.logging.Logger;
+import io.zbus.kit.logging.LoggerFactory;
 import io.zbus.net.EventLoop; 
 
 public class HttpInvoker {
