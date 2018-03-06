7b1300c5b409c5ecfc24b7aa0c4c664887b3dd44
rushmore <44194462@qq.com>
2018-03-06 14:39:55 +0800

    update

--- src/test/java/io/zbus/net/http/HttpInvokerExample.java
+++ src/test/java/io/zbus/net/http/HttpInvokerExample.java
@@ -7,11 +7,11 @@ public class HttpInvokerExample {
 	public static void main(String[] args) throws Exception, InterruptedException {    
 		EventLoop loop = new EventLoop();
 		
-		HttpInvoker http = new HttpInvoker("https://api.binance.com", loop);
+		HttpInvoker http = new HttpInvoker(loop);
 		
 		HttpMsg req = new HttpMsg();  
-		req.setUrl("/api/v1/exchangeInfo");    
-		Object resp = http.json(req, 10000);
+		req.setUrl("https://api.binance.com/api/v1/exchangeInfo");    
+		Object resp = http.json(req);
 		
 		System.out.println(resp);
 		
