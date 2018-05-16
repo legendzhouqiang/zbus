6d040abf7923a3842f27ab028f9372dbba4ef926
rushmore <44194462@qq.com>
2018-05-16 10:32:49 +0800

    update jvm example

--- src/test/java/org/zbus/examples/broker/JvmBrokerExample.java
+++ src/test/java/org/zbus/examples/broker/JvmBrokerExample.java
@@ -1,38 +1,41 @@
 package org.zbus.examples.broker;
 
 import java.io.IOException;
-
+ 
 import org.zbus.broker.Broker;
 import org.zbus.broker.JvmBroker;
+import org.zbus.kit.log.Logger;
+import org.zbus.kit.log.LoggerFactory;
 import org.zbus.mq.Consumer;
 import org.zbus.mq.Consumer.ConsumerHandler;
 import org.zbus.mq.Producer;
 import org.zbus.net.http.Message;
 
 public class JvmBrokerExample {
-
+	private static final Logger logger = LoggerFactory.getLogger(JvmBrokerExample.class);
 	public static void main(String[] args) throws Exception {  
 		Broker broker = new JvmBroker();
 		
 		Consumer consumer = new Consumer(broker, "MyMQ");  
+		consumer.createMQ();
 		consumer.start(new ConsumerHandler() { 
 			@Override
 			public void handle(Message msg, Consumer consumer) throws IOException { 
-				System.out.println(msg);
+				logger.info(""+msg);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) { 
+					e.printStackTrace();
+				}
 			}
 		});  
 		
 		Producer producer = new Producer(broker, "MyMQ");
-		Message message = new Message();
-		message.setBody("test body");
-		producer.sendAsync(message);
-		
-		Thread.sleep(100);
-		System.out.println("destroy consumer");
-		consumer.close();
-		System.out.println("destroy broker");
-		broker.close(); 
-		System.out.println("destroyed environment");
+		for(int i=0;i<2;i++) {
+			Message message = new Message();
+			message.setBody("test body");
+			producer.sendAsync(message); 
+		} 
 		
 	} 
 	
